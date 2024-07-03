f5b101911652937bd6cd09afbd472e9b458e98bc
RD <romas@pickvibe.com>
2024-07-03 17:00:12 +0300

    Add BaseEntity abstract class
    Created a new abstract class named BaseEntity in the Entity directory. This class includes properties, constructor, getters and setters for both id and uuid fields. UUID is made a unique field and it's initialized in the constructor.

---
+++ src/Entity/BaseEntity.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Metadata\ApiProperty;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Types\UuidType;
+use Symfony\Component\Serializer\Annotation\Groups;
+use Symfony\Component\Uid\Uuid;
+
+abstract class BaseEntity
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue(strategy: "SEQUENCE")]
+    #[ORM\Column]
+    #[ApiProperty(identifier: false)]
+    protected ?int $id = null;
+
+    #[ORM\Column(type: UuidType::NAME, unique: true)]
+    #[ApiProperty(identifier: true)]
+    #[Groups(['flight:read', 'skill:read'])]
+    protected Uuid $uuid;
+
+    public function __construct()
+    {
+        $this->uuid = Uuid::v4();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getUuid(): Uuid
+    {
+        return $this->uuid;
+    }
+
+    public function setUuid(Uuid $uuid): static
+    {
+        $this->uuid = $uuid;
+        return $this;
+    }
+}
