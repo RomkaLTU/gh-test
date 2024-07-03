7be4fec0a368777e9e9ea5f36d5685b71d3b37b7
RD <romas@pickvibe.com>
2024-07-03 21:50:44 +0300

    Add many-to-many relation between GroundCrewMember and Skill
    The entities GroundCrewMember and Skill are updated to define a many-to-many relationship between them. The entities are also modified to add and remove such relations. Accompanying setters and getters are also provided to manage these relationships. These changes enable the application to efficiently capture and manipulate multiple skillsets for each GroundCrewMember and conversely the crew members related to each skill.

--- src/Entity/GroundCrewMember.php
+++ src/Entity/GroundCrewMember.php
@@ -2,6 +2,7 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Metadata\ApiProperty;
 use ApiPlatform\Metadata\ApiResource;
 use ApiPlatform\Metadata\Get;
 use ApiPlatform\Metadata\Post;
@@ -9,6 +10,8 @@ use App\Repository\GroundCrewMemberRepository;
 use App\State\GroundCrewMemberStateProcessor;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Serializer\Annotation\Groups;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 
 #[ORM\Entity(repositoryClass: GroundCrewMemberRepository::class)]
 #[ApiResource(
@@ -25,8 +28,28 @@ use Symfony\Component\Serializer\Annotation\Groups;
 class GroundCrewMember extends BaseEntity
 {
     #[ORM\Column(length: 255)]
+    #[ApiProperty(openapiContext: [
+        'example' => 'John Doe'
+    ])]
     #[Groups(['gcm:read', 'gcm:write'])]
     private ?string $name = null;
+
+    #[ORM\ManyToMany(targetEntity: Skill::class, inversedBy: 'groundCrewMembers')]
+    #[ORM\JoinTable(name: 'ground_crew_member_skills')]
+    #[ApiProperty(openapiContext: [
+        'example' => [
+            '/api/skills/d97fd14b-0d71-4565-8d28-e62a34b5b0d2',
+        ]
+    ])]
+    #[Groups(['gcm:read', 'gcm:write'])]
+    private Collection $skills;
+
+    public function __construct()
+    {
+        parent::__construct();
+
+        $this->skills = new ArrayCollection();
+    }
 
     public function getName(): ?string
     {
@@ -36,6 +59,30 @@ class GroundCrewMember extends BaseEntity
     public function setName(string $name): static
     {
         $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Skill>
+     */
+    public function getSkills(): Collection
+    {
+        return $this->skills;
+    }
+
+    public function addSkill(Skill $skill): static
+    {
+        if (!$this->skills->contains($skill)) {
+            $this->skills->add($skill);
+        }
+
+        return $this;
+    }
+
+    public function removeSkill(Skill $skill): static
+    {
+        $this->skills->removeElement($skill);
 
         return $this;
     }
--- src/Entity/Skill.php
+++ src/Entity/Skill.php
@@ -2,11 +2,14 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Metadata\ApiProperty;
 use ApiPlatform\Metadata\ApiResource;
 use ApiPlatform\Metadata\Get;
 use ApiPlatform\Metadata\Post;
 use App\Repository\SkillRepository;
 use App\State\SkillStateProcessor;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Serializer\Annotation\Groups;
 
@@ -22,8 +25,52 @@ use Symfony\Component\Serializer\Annotation\Groups;
 class Skill extends BaseEntity
 {
     #[ORM\Column(length: 255)]
-    #[Groups(['skill:read', 'skill:write'])]
+    #[ApiProperty(openapiContext: [
+        'example' => 'Piloting'
+    ])]
+    #[Groups(['skill:read', 'skill:write', 'gcm:read'])]
     private ?string $name = null;
+
+    #[ORM\ManyToMany(targetEntity: GroundCrewMember::class, mappedBy: 'skills')]
+    #[ApiProperty(openapiContext: [
+        'example' => [
+            '/api/ground-crew-members/0bae7248-76a3-4603-94a8-b1c2d410b8a2',
+        ]
+    ])]
+    #[Groups(['skill:read', 'skill:write'])]
+    private Collection $groundCrewMembers;
+
+    public function __construct()
+    {
+        parent::__construct();
+
+        $this->groundCrewMembers = new ArrayCollection();
+    }
+
+    /**
+     * @return Collection<int, GroundCrewMember>
+     */
+    public function getGroundCrewMembers(): Collection
+    {
+        return $this->groundCrewMembers;
+    }
+
+    public function addGroundCrewMember(GroundCrewMember $groundCrewMember): static
+    {
+        if (!$this->groundCrewMembers->contains($groundCrewMember)) {
+            $this->groundCrewMembers->add($groundCrewMember);
+            $groundCrewMember->addSkill($this);
+        }
+
+        return $this;
+    }
+
+    public function removeGroundCrewMember(GroundCrewMember $groundCrewMember): static
+    {
+        $this->groundCrewMembers->removeElement($groundCrewMember);
+
+        return $this;
+    }
 
     public function getName(): ?string
     {
