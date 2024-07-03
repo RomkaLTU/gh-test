d4bffec88b0da804cc42b7c454479fba7a87662a
RD <romas@pickvibe.com>
2024-07-03 16:12:18 +0300

    Add cache clear and database reset to test bootstrap

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -11,3 +11,18 @@ if (method_exists(Dotenv::class, 'bootEnv')) {
 if ($_SERVER['APP_DEBUG']) {
     umask(0000);
 }
+
+passthru(sprintf(
+    'APP_ENV=%s php "%s/../bin/console" cache:clear --no-warmup',
+    $_ENV['APP_ENV'],
+    __DIR__
+));
+
+passthru(sprintf(
+    'php "%s/../bin/console" doctrine:schema:drop --full-database --force --env=test',
+    __DIR__
+));
+passthru(sprintf(
+    'php "%s/../bin/console" doctrine:schema:update --force --env=test',
+    __DIR__
+));
