2dc86ed3161acc07e120ff3e195cc4f4b8af2c49
RD <romas@pickvibe.com>
2024-07-03 16:13:14 +0300

    Add FlightStateProcessor class
    This commit introduces the FlightStateProcessor class, which implements the ProcessorInterface. This class handles processing of the Flight entity with functions such as persisting data and flushing it to the database. It also validates if the data provided is an instance of Flight.

---
+++ src/State/FlightStateProcessor.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\State;
+
+use ApiPlatform\Metadata\Operation;
+use ApiPlatform\State\ProcessorInterface;
+use App\Entity\Flight;
+use Doctrine\ORM\EntityManagerInterface;
+use InvalidArgumentException;
+
+readonly class FlightStateProcessor implements ProcessorInterface
+{
+    public function __construct(private EntityManagerInterface $entityManager)
+    {}
+
+    /**
+     * @return Flight|void
+     */
+    public function process(mixed $data, Operation $operation, array $uriVariables = [], array $context = []): mixed
+    {
+        if (!$data instanceof Flight) {
+            throw new InvalidArgumentException('Data is not an instance of Flight');
+        }
+
+        $this->entityManager->persist($data);
+        $this->entityManager->flush();
+
+        return $data;
+    }
+}
