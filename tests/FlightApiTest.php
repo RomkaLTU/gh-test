ad161c81dc918a8425f7165b265358450dc42c1b
RD <romas@pickvibe.com>
2024-07-03 16:12:49 +0300

    Add FlightApiTest class for API testing
    A FlightApiTest class has been introduced to ensure proper testing of the flight API's endpoints. It includes methods to test creating a flight, fetching an existing flight, and correctly handling non-existent flights.

---
+++ tests/FlightApiTest.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Tests;
+
+use ApiPlatform\Symfony\Bundle\Test\ApiTestCase;
+use ApiPlatform\Symfony\Bundle\Test\Client;
+use Symfony\Component\HttpFoundation\Response;
+
+class FlightApiTest extends ApiTestCase
+{
+    private Client $client;
+
+    protected function setUp(): void
+    {
+        $this->client = static::createClient();
+    }
+
+    public function testCreateFlight(): array
+    {
+        $flightData = [
+            'nr' => 'FL123',
+        ];
+
+        $response = $this->client->request('POST', '/api/flights', [
+            'headers' => ['Content-Type' => 'application/ld+json'],
+            'json' => $flightData,
+        ]);
+
+        $this->assertResponseStatusCodeSame(Response::HTTP_CREATED);
+        $this->assertJsonContains([
+            'nr' => 'FL123',
+        ]);
+
+        return $response->toArray();
+    }
+
+    /**
+     * @depends testCreateFlight
+     */
+    public function testGetExistingFlight(array $createdFlight): void
+    {
+        $uuid = $createdFlight['uuid'];
+
+        $this->client->request('GET', '/api/flights/' . $uuid);
+
+        $this->assertResponseStatusCodeSame(Response::HTTP_OK);
+
+        $this->assertJsonContains([
+            '@context' => '/api/contexts/Flight',
+            '@id' => '/api/flights/' . $uuid,
+            '@type' => 'Flight',
+            'uuid' => $uuid,
+            'nr' => 'FL123',
+        ]);
+    }
+
+    public function testGetNonExistentFlight(): void
+    {
+        $nonExistentUuid = 'c022460d-2fcd-4ed1-a246-0d2fcdced147';
+        $this->client->request('GET', '/api/flights/' . $nonExistentUuid);
+
+        $this->assertResponseStatusCodeSame(Response::HTTP_NOT_FOUND);
+    }
+}
