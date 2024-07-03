769f3379bdd9683baaf6cfd7eae117f03ffc7373
RD <romas@pickvibe.com>
2024-07-03 16:13:40 +0300

    Update Flight entity with API Platform features
    ApiPlatform metadata and resources are added to the Flight entity. This includes operations for 'Get' and 'Post' with the appropriate URI templates and processors. Further, ORM mapping and properties have been updated for id and uuid to improve data handling.

--- src/Entity/Flight.php
+++ src/Entity/Flight.php
@@ -2,22 +2,41 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Metadata\ApiProperty;
+use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\Get;
+use ApiPlatform\Metadata\Post;
 use App\Repository\FlightRepository;
+use App\State\FlightStateProcessor;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Types\UuidType;
+use Symfony\Component\Serializer\Annotation\Groups;
 use Symfony\Component\Uid\Uuid;
 
 #[ORM\Entity(repositoryClass: FlightRepository::class)]
+#[ApiResource(
+    operations: [
+        new Get(uriTemplate: '/flights/{uuid}'),
+        new Post(processor: FlightStateProcessor::class)
+    ],
+    normalizationContext: ['groups' => ['flight:read']],
+    denormalizationContext: ['groups' => ['flight:write']],
+)]
 class Flight
 {
     #[ORM\Id]
-    #[ORM\GeneratedValue]
+    #[ORM\GeneratedValue(strategy: "SEQUENCE")]
     #[ORM\Column]
+    #[ApiProperty(identifier: false)]
     private ?int $id = null;
 
-    #[ORM\Column(type: 'uuid', unique: true)]
-    private ?Uuid $uuid = null;
+    #[ORM\Column(type: UuidType::NAME, unique: true)]
+    #[ApiProperty(identifier: true)]
+    #[Groups(['flight:read'])]
+    private Uuid $uuid;
 
     #[ORM\Column(length: 255)]
+    #[Groups(['flight:read', 'flight:write'])]
     private string $nr;
 
     public function __construct()
@@ -25,17 +44,17 @@ class Flight
         $this->uuid = Uuid::v4();
     }
 
-    public function getId(): ?int
+    public function getId(): string
     {
         return $this->id;
     }
 
-    public function getUuid(): ?Uuid
+    public function getUuid(): Uuid
     {
         return $this->uuid;
     }
 
-    public function getNr(): string
+    public function getNr(): ?string
     {
         return $this->nr;
     }
@@ -43,6 +62,13 @@ class Flight
     public function setNr(string $nr): static
     {
         $this->nr = $nr;
+
+        return $this;
+    }
+
+    public function setUuid(Uuid $uuid): self
+    {
+        $this->uuid = $uuid;
 
         return $this;
     }
