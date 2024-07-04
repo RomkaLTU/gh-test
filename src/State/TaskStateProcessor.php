7387459d293addb9c45da930dc6a501c07472127
RD <hello@codesomelabs.com>
2024-07-04 10:10:46 +0300

    Add task validation in TaskStateProcessor
    TaskStateProcessor has been updated with a new validation method to ensure the assigned member has the required skills and certifications for a specified task. UnprocessableEntityHttpException will be thrown if a mismatch is found or if the certification has expired.

--- src/State/TaskStateProcessor.php
+++ src/State/TaskStateProcessor.php
@@ -4,9 +4,12 @@ namespace App\State;
 
 use ApiPlatform\Metadata\Operation;
 use ApiPlatform\State\ProcessorInterface;
+use App\Entity\GroundCrewMember;
 use App\Entity\Task;
+use DateTimeImmutable;
 use Doctrine\ORM\EntityManagerInterface;
 use InvalidArgumentException;
+use Symfony\Component\HttpKernel\Exception\UnprocessableEntityHttpException;
 
 readonly class TaskStateProcessor implements ProcessorInterface
 {
@@ -19,9 +22,65 @@ readonly class TaskStateProcessor implements ProcessorInterface
             throw new InvalidArgumentException('Data is not an instance of Task');
         }
 
+        $this->validateTask($data);
+
         $this->entityManager->persist($data);
         $this->entityManager->flush();
 
         return $data;
+    }
+
+    private function validateTask(Task $task): void
+    {
+        $assignedMember = $task->getAssignedTo();
+
+        if (!$assignedMember) {
+            return;
+        }
+
+        $this->validateSkills($task, $assignedMember);
+        $this->validateCertifications($task, $assignedMember);
+    }
+
+    private function validateSkills(Task $task, GroundCrewMember $assignedMember): void
+    {
+        $requiredSkills = $task->getRequiredSkills();
+        $memberSkills = $assignedMember->getSkills();
+
+        foreach ($requiredSkills as $skill) {
+            if (!$memberSkills->contains($skill)) {
+                throw new UnprocessableEntityHttpException(sprintf(
+                    'Assigned member does not have required skill: %s',
+                    $skill->getName()
+                ));
+            }
+        }
+    }
+
+    private function validateCertifications(Task $task, GroundCrewMember $assignedMember): void
+    {
+        $requiredCertifications = $task->getRequiredCertifications();
+        $memberCertifications = $assignedMember->getCertifications();
+        $now = new DateTimeImmutable();
+
+        foreach ($requiredCertifications as $cert) {
+            $memberCert = $memberCertifications->filter(function($c) use ($cert) {
+                return $c->getId() === $cert->getId();
+            })->first();
+
+            if (!$memberCert) {
+                throw new UnprocessableEntityHttpException(sprintf(
+                    'Assigned member does not have required certification: %s',
+                    $cert->getName()
+                ));
+            }
+
+            if ($memberCert->getValidityDate() <= $now) {
+                throw new UnprocessableEntityHttpException(sprintf(
+                    'Assigned member\'s certification has expired: %s',
+                    $cert->getName()
+                ));
+            }
+        }
     }
 }
